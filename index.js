3ed74f5862abcce6c3f5c8ec68c7f55b6c7b5c12
noobchirag69 <chiragchakraborty48@gmail.com>
2022-08-04 13:34:39 +0530

    Minor Change

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3000 || process.env.PORT;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Listen on a port
+server.listen(3000);
